3545354984c5b1d2d470ec83af44e4be41ef867f
fcourt <108086687+fcourt@users.noreply.github.com>
2026-04-02 20:45:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,20 @@ export default defineConfig({
   resolve: {
     alias: { src: '/src' },
   },
+  optimizeDeps: {
+    include: ['extended-typescript-sdk'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/extended-typescript-sdk/, /node_modules/],
+    },
+  },
   server: {
     proxy: {
       '/api': {
         target: 'https://api.extended.exchange',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/extended\?endpoint=/, ''),
+        rewrite: (path) => path.replace(/^\\/api\\/extended\\?endpoint=/, ''),
       },
     },
   },
